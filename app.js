44768f31a2915f9cdd16e9ad131010e529aac696
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2024-08-06 13:57:22 +0530

    configured cors

--- app.js
+++ app.js
@@ -16,10 +16,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use(cors({
+    origin: "https://bitem-business.vercel.app",
+    methods: "POST, GET",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 app.use(express.json());
-app.use(cors());
 app.use(express.urlencoded({extended: true}));
 
 app.use('/', homeRoute);
