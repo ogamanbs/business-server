31b7d3c0f541c4df1832b1e5036b9c00d62c651e
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2024-08-06 14:24:55 +0530

    added preflight allowed cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(cors({
+app.use('*', cors({
     origin: "https://bitem-business.vercel.app",
     methods: "POST, GET",
     preflightContinue: false,
