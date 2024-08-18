04d2cd2c63a441fd267aec10d6c7eae7bf8004c8
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2024-08-18 14:37:04 +0530

    changed create products logic a bit

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,29 +2,24 @@ const ProductModel = require('../models/product-model');
 const OwnerModel = require('../models/owner-model');
 
 module.exports.create = async (req, res, next) => {
-    const { image, name, price, discount, bgcolor, panelColor, textColor, email} = req.body;
-    const owner = await OwnerModel.findOne({email});
-    if(owner) {
-        const createdProduct = await ProductModel.create({
-            image,
-            name,
-            price,
-            discount,
-            bgcolor,
-            panelColor,
-            textColor,
-            owner: owner._id
-        });
-        if(createdProduct) {
-            if(owner.products.indexOf(createdProduct.name) === -1) {
-                owner.products.push(createdProduct._id);
-                await owner.save();
-                res.status(200).json({message: `${name} successfully created`});
-            } else {
-                res.status(401).json({message: '${name} already present in owner store'});
-            }
+    const { image, name, price, discount, bgcolor, panelColor, textColor, id} = req.body;
+    const createdProduct = await ProductModel.create({
+        image,
+        name,
+        price,
+        discount,
+        bgcolor,
+        panelColor,
+        textColor,
+        owner: id
+    });
+    if(createdProduct) {
+        if(owner.products.indexOf(createdProduct.name) === -1) {
+            owner.products.push(createdProduct._id);
+            await owner.save();
+            res.status(200).json({message: `${name} successfully created`});
         } else {
-            res.status(401).json({message: 'error creating product'});
+            res.status(401).json({message: '${name} already present in owner store'});
         }
     } else {
         res.status(401).json({message: 'error creating product'});
