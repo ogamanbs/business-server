17159812fa8e09deabe33658b1d226209e673308
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2024-08-18 15:53:13 +0530

    changed get owner route name

--- routes/ownerRoute.js
+++ routes/ownerRoute.js
@@ -5,7 +5,7 @@ const { create, login, getOwner } = require('../controllers/ownerAuthController'
 router.get('/', (req, res, next) => {
     res.send('owner');
 })
-router.post('/getOwner', getOwner);
+router.post('/get-owner', getOwner);
 router.post('/create', create);
 router.post('/login', login);
 
