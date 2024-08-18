4ea3d86ed23f40ca28b104aaeb056d25b4d2d21f
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2024-08-18 18:44:14 +0530

    changed the product creation logic

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,26 +3,34 @@ const OwnerModel = require('../models/owner-model');
 
 module.exports.create = async (req, res, next) => {
     const { image, name, price, discount, bgcolor, panelColor, textColor, id} = req.body;
-    const createdProduct = await ProductModel.create({
-        image,
-        name,
-        price,
-        discount,
-        bgcolor,
-        panelColor,
-        textColor,
-        owner: id
-    });
-    if(createdProduct) {
-        if(owner.products.indexOf(createdProduct.name) === -1) {
+    const owner = await OwnerModel.findOne({_id:id}).populate('products');
+    let flag = 0;
+    for(let i = 0; i<owner.products.length; i++) {
+        if(owner.products[i].name === name) {
+            flag = 1;
+            break;
+        }
+    }
+    if(flag === 0) {
+        const createdProduct = await ProductModel.create({
+            image,
+            name,
+            price,
+            discount,
+            bgcolor,
+            panelColor,
+            textColor,
+            owner: id
+        });
+        if(createdProduct) {
             owner.products.push(createdProduct._id);
             await owner.save();
             res.status(200).json({message: `${name} successfully created`});
         } else {
-            res.status(401).json({message: '${name} already present in owner store'});
+            res.status(401).json({message: 'error creating product'});
         }
     } else {
-        res.status(401).json({message: 'error creating product'});
+        res.status(401).json({message: 'product already exists in your store'});
     }
 }
 
